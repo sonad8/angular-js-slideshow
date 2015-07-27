2b19d31494cba37bffc150f73803b1706eaee2c3
Joseph Chen <joseph.chen@gmail.com>
2015-07-27 07:16:28 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var app = angular.module('angular',[]);
+
+app.directive('slideit',function() {
+    return {
+       restrict: 'A',
+       replace: true,
+       scope: {
+         slideit: '=' 
+       },
+       template: '<ul class="bxslider">' +
+                  '<li ng-repeat="slide in slides">' +
+                 '<img ng-src="{{slide.src}}" alt="" />' +
+                 '</li>' +
+                 '</ul>',
+       link: function(scope, elm, attrs) {
+          elm.ready(function() {
+            scope.$apply(function() {
+                scope.slides = scope.slideit;
+            });                    
+            elm.bxSlider({
+              adaptiveHeight: true,
+              mode: 'fade'});   
+            });
+       }
+    }; 
+});
+
+app.controller('MainCtrl', function($scope) {
+  $scope.base = 'http://image.tmdb.org/';
+  $scope.images = [
+       {src: $scope.base + 't/p/w1280/8y2IcNwQBuyuud3YO8IP41xUgEz.jpg' },
+       {src: $scope.base + 't/p/w1280/2BXd0t9JdVqCp9sKf6kzMkr7QjB.jpg' },
+       {src: $scope.base + 't/p/w1280/oYZtd7Md78HuzBvT5dlJlHWc89O.jpg' }
+     ];
+});
